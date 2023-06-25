3294c4a3f1843f3dda49aa0cddaf0271bb3172d3
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-06-25 17:04:49 +0900

    Revert "마크업 통합 + 로그인페이지 css"
    This reverts commit 17ad4b3aa3a4592dd30eee79b133b6a771e4366e.

--- src/js/form__validation.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll("input").forEach((input) => {
-  input.addEventListener("invalid", (e) => {
-    e.target.classList.add("is--invalid");
-  });
-  input.addEventListener("valid", (e) => {
-    e.target.classList.remove("is--invalid");
-  });
-});
