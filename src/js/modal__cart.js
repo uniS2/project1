00c14435990253c6862ef5eee5fa76322ac43d7b
uniS2 <s2oie1010@gmail.com>
2023-06-27 23:11:33 +0900

    모달 및 팝업창 마크업 및 스타일링 완료

--- src/js/modal__cart.js
+++ src/js/modal__cart.js
@@ -1,16 +1,15 @@
+// 모달창
 function showModal() {
-  document.querySelector(".background").classList.add("show");
+  document.querySelector(".modal__background").classList.add("show");
   document.body.style.overflow = "hidden";
 }
 
 function closeModal(event) {
   event.preventDefault(); // 기본 동작 취소
-  document.querySelector(".background").classList.remove("show");
-  document.body.style.overflow = "auto";  // 스크롤 활성화
+  document.querySelector(".modal__background").classList.remove("show");
+  document.body.style.overflow = "auto"; // 스크롤 활성화
 }
 
-document.querySelector("#show").addEventListener("click", showModal);
+document.querySelector(".show").addEventListener("click", showModal);
 
-document.querySelector("#close").addEventListener("click", closeModal);
-
-// event: Event
+document.querySelector(".close").addEventListener("click", closeModal);
